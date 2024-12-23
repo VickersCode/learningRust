90d913ad5ce081c9eff0647284496212f51549c4
Tyler Vickers <tyler31vickers@gmail.com>
2024-12-23 13:23:30 -0800

    working with cargo

---
+++ hello_cargo/target/debug/deps/hello_cargo-5a2214154ee677e7.d
@@ -0,0 +1,5 @@
+C:\projects\learningRust\hello_cargo\target\debug\deps\hello_cargo-5a2214154ee677e7.exe: src/main.rs
+
+C:\projects\learningRust\hello_cargo\target\debug\deps\hello_cargo-5a2214154ee677e7.d: src/main.rs
+
+src/main.rs:
---
+++ hello_cargo/target/debug/deps/hello_cargo-b9239c39f36b5701.d
@@ -0,0 +1,5 @@
+C:\projects\learningRust\hello_cargo\target\debug\deps\libhello_cargo-b9239c39f36b5701.rmeta: src/main.rs
+
+C:\projects\learningRust\hello_cargo\target\debug\deps\hello_cargo-b9239c39f36b5701.d: src/main.rs
+
+src/main.rs:
